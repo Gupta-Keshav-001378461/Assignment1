d75b5ced88e60dd566dae68326becfd5a4a3f1e4
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2020-01-18 22:57:06 -0500

    Create profile assignment

---
+++ src/Business/Profile.java
@@ -0,0 +1,237 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author keshav
+ */
+public class Profile {
+       private String f_name;
+    private String l_name;
+    private String p_num;
+    private String dob;
+    private String age;
+    private String ssn;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String bankName;
+    private String acctype;
+    private String balance;
+    private String accNumber;
+    private String routingNum;
+    private String bankName1;
+    private String acctype1;
+    private String balance1;
+    private String accNumber1;
+    private String mrn;
+    private String allergy1;
+    private String allergy2;
+    private String allergy3;
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getMrn() {
+        return mrn;
+    }
+
+    public void setMrn(String mrn) {
+        this.mrn = mrn;
+    }
+
+    public String getAllergy1() {
+        return allergy1;
+    }
+
+    public void setAllergy1(String allergy1) {
+        this.allergy1 = allergy1;
+    }
+
+    public String getAllergy2() {
+        return allergy2;
+    }
+
+    public void setAllergy2(String allergy2) {
+        this.allergy2 = allergy2;
+    }
+
+    public String getAllergy3() {
+        return allergy3;
+    }
+
+    public void setAllergy3(String allergy3) {
+        this.allergy3 = allergy3;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAcctype() {
+        return acctype;
+    }
+
+    public void setAcctype(String acctype) {
+        this.acctype = acctype;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+    public String getRoutingNum() {
+        return routingNum;
+    }
+
+    public void setRoutingNum(String routingNum) {
+        this.routingNum = routingNum;
+    }
+
+    public String getBankName1() {
+        return bankName1;
+    }
+
+    public void setBankName1(String bankName1) {
+        this.bankName1 = bankName1;
+    }
+
+    public String getAcctype1() {
+        return acctype1;
+    }
+
+    public void setAcctype1(String acctype1) {
+        this.acctype1 = acctype1;
+    }
+
+    public String getBalance1() {
+        return balance1;
+    }
+
+    public void setBalance1(String balance1) {
+        this.balance1 = balance1;
+    }
+
+    public String getAccNumber1() {
+        return accNumber1;
+    }
+
+    public void setAccNumber1(String accNumber1) {
+        this.accNumber1 = accNumber1;
+    }
+
+    public String getRoutingNum1() {
+        return routingNum1;
+    }
+
+    public void setRoutingNum1(String routingNum1) {
+        this.routingNum1 = routingNum1;
+    }
+    private String routingNum1;
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public String getP_num() {
+        return p_num;
+    }
+
+    public void setP_num(String p_num) {
+        this.p_num = p_num;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+}
